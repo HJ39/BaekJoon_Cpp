16adca0a617ad12f9da90b24ebc05596bdd572ac
HJ39 <81843677+HJ39@users.noreply.github.com>
2022-02-27 15:41:37 +0900

    BaekJoon 2751 수 정렬하기2

--- BaekJoon_Cpp/2751_Main.cpp
+++ BaekJoon_Cpp/2751_Main.cpp
@@ -2,24 +2,20 @@
 #include <algorithm>
 #include <vector>
 
+// cin,cout보다 scanf,printf가 더 속도가 빠르다
+
 using namespace std;
 int main() {
-	
 	int N;
-	cin >> N;
-
-	vector<int> vn;
-	
+	cin >> N;	
+	vector<int> vn;	
 	for (int i = 0; i < N; i++) {
 		int n;
-		cin >> n;
+		scanf_s("%d", &n);	
 		vn.push_back(n);
 	}
-
 	sort(vn.begin(), vn.end());		//cpp STL sort함수는 시간 복잡도가 O(nlogn)이다
-
-	for (int i = 0; i < N; i++)
-		cout << vn[i] << endl;
-	
+	for (int i = 0; i <N ; i++)
+		printf("%d\n", vn[i]);
 	return 0;
 }
