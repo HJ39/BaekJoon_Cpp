bed4cd69fad0fcee53592a36ffda70ebd1fd9fe6
HJ39 <81843677+HJ39@users.noreply.github.com>
2022-03-02 01:06:55 +0900

    BaekJoon 15652 N과 M(4)

---
+++ BaekJoon_Cpp/15652_Main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#define MAX 8
+using namespace std;
+
+int N, M, cnt;
+vector<int> vn(MAX);
+bool check[MAX];
+
+void DFS(int cnt) {
+	if (cnt == M) {
+		for (int i = 0; i < M; i++)
+			printf("%d ", vn[i]);
+		printf("\n");
+		return;
+	}
+	for (int i = 0; i < N; i++) {
+		if (cnt > 0 && vn[cnt - 1] > i + 1)
+			continue;
+		vn[cnt] = i + 1;
+		DFS(cnt + 1);
+	}
+}
+int main() {
+	cin >> N >> M;
+	DFS(0);
+	return 0;
+}
