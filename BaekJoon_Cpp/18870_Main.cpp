2e0351f786669e861a3fba1a057d471d94e97fb1
HJ39 <81843677+HJ39@users.noreply.github.com>
2022-03-01 15:47:18 +0900

    BaekJoon 18870 좌표 압축 (수정1)

--- BaekJoon_Cpp/18870_Main.cpp
+++ BaekJoon_Cpp/18870_Main.cpp
@@ -24,5 +24,36 @@ int main() {
 		result = lower_bound(tn.begin(), tn.end(), (*it));		
 		printf("%d ", result - tn.begin());
 	}	
+
+	/*	알고리즘 알아두기
+	int n;
+	cin >> n;
+	vector<pair<int, int>> arr(n);
+	vector<int> result(n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i].first;
+		arr[i].second = i;
+	}
+	sort(arr.begin(), arr.end(), less<pair<int, int>>());
+	//개수 미리 세두기
+	int pre = arr[0].first;
+	int Count = 0;
+	for (int i = 0; i < n; i++)
+	{		
+		if (pre != arr[i].first) {			
+			Count++;
+			pre = arr[i].first;
+		}
+		result[arr[i].second] = Count;
+	}
+	//입력순서대로 값저장되어 있음
+	for (int i = 0; i < n; i++)
+	{
+		cout << result[i] << " ";
+	}	
+	*/
+
+
 	return 0;
 }
